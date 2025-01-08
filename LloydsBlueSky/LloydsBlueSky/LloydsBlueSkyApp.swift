4dc0d2fc70da8aee5717747871112d5e984ffe0d
John Shave <johnshave@gmail.com>
2025-01-08 21:38:24 +0000

    Initial project files

---
+++ LloydsBlueSky/LloydsBlueSky/LloydsBlueSkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LloydsBlueSkyApp.swift
+//  LloydsBlueSky
+//
+//  Created by John Shave on 08/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct LloydsBlueSkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
